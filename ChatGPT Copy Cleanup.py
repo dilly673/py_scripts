8e67ba1596c33b8f388e22694b62d2f6de7a8c49
dilly673 <72178955+dilly673@users.noreply.github.com>
2024-07-16 22:54:16 -0500

    Add files via upload

---
+++ ChatGPT Copy Cleanup.py
@@ -0,0 +1,24 @@
+
+
+def chatgptcleanup(input_char):
+
+    gpt_cleanup = ""
+
+    for symbol in input_char:
+        if symbol != '*' and symbol != "#":
+            gpt_cleanup += symbol
+             
+    return gpt_cleanup
+         
+
+#Example
+
+copy_gpt = '''
+
+   insert text data here _ Mon**aj#jk*kjsd## _ run to test
+
+'''
+
+updated_cleanup = chatgptcleanup(copy_gpt)
+
+print (updated_cleanup)
